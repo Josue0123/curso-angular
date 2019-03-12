1d612ea0c077a0932f93d85b7ac68f3cecd0b25f
Josue0123 <josueyepezponce@hotmail.com>
2019-03-12 17:35:55 -0600

    Se agregó un modelo y se modificó otra madre

--- src/app/models/configuracion.ts
+++ src/app/models/configuracion.ts
@@ -1,6 +1,7 @@
 export var Configuracion = {
     color : "red",
     fondo : "#eee",
-    titulo : "Master con JavaScript y Angular",
+    titulo : "Master con JavaScript y Angular, TypeScript.",
     descripcion: "Curso de Angular"
+
 };
